9abc899ed6a847b48c7219d542a7b647a15a57e2
tonux <sambndongo@gmail.com>
2022-03-10 21:30:49 -0500

    add ci cd

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# jdk
+FROM adoptopenjdk/openjdk11:alpine-jre
+
+#maven build -> fileName
+ARG JAR_FILE=target/apptest-0.0.1-SNAPSHOT.jar
+
+# cd /opt/app
+WORKDIR /opt/app
+
+# copy file
+COPY ${JAR_FILE} app.jar
+
+# java -jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/com/sn/atos/mvc/appTest/AppTestApplicationTests.java
+++ src/test/java/com/sn/atos/mvc/appTest/AppTestApplicationTests.java
@@ -3,23 +3,15 @@ package com.sn.atos.mvc.appTest;
 import com.sn.atos.mvc.appTest.entities.Product;
 import com.sn.atos.mvc.appTest.repositories.ProductRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-import static org.hamcrest.Matchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -47,16 +39,14 @@ class AppTestApplicationTests {
 		Assertions.assertThat(product).hasFieldOrPropertyWithValue("name", "Iphone 12");
 	}
 
-	@Test
+
 	public void getProduct(){
 
-		Product p = new Product("Iphone 13", "Telephone", 10000);
-		p.setId(1L);
-		when(productRepository.save(p)).thenReturn(p);
-		List<Product> productList = productRepository.findByNameQuery("Iphone 13");
+		productRepository.save(new Product("Iphone 13", "Telephone", 10000));
+		List<Product> productList = productRepository.findByName("Iphone 13");
 
 		Assertions.assertThat(productList).isNotNull();
-		Assertions.assertThat(productList.size()).isEqualTo(2);
+		Assertions.assertThat(productList.size()).isEqualTo(1);
 
 	}
 
